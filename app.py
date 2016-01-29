0be14654595d81e630e3959ec9638001c36e2eea
Nancy Wong <nancypwong@gmail.com>
2016-01-29 01:33:42 -0500

    Bind to PORT and set default

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
+
 from flask import Flask, render_template
+
 import ling
 
 app = Flask(__name__)
@@ -28,4 +31,6 @@ def generate():
     return lines
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
